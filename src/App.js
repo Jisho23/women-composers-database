7c18c1a632f036673e392974bfcbee9b6e04b154
Joshua Denenberg <j.denenberg42@gmail.com>
2018-01-29 09:49:08 -0500

    initial test of JSON and list of composers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import { List } from "semantic-ui-react";
 
 class App extends Component {
-  state = {};
+  state = {
+    composers: {},
+    filters: {
+      name: ""
+    }
+  };
   componentDidMount() {
-    var json = require('./data.json')
+    const json = require("./data.json");
+    const composerList = json["Women Composers Database"];
+    this.setState({ composers: composerList });
     debugger;
   }
   render() {
@@ -14,11 +21,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to the Women Composers Database</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <List className="App-intro">
+          {Object.keys(this.state.composers).map(composer => (
+            <List.Item>{composer}</List.Item>
+          ))}
+        </List>
       </div>
     );
   }
