db1ed96340c958adc6335dc7760c01c2e0f09af7
Joshua Denenberg <j.denenberg42@gmail.com>
2018-02-26 12:46:40 -0500

    minor change, re-added form inversion

--- src/components/helpers/FilterForm.js
+++ src/components/helpers/FilterForm.js
@@ -9,85 +9,87 @@ export const createAdvancedFilterForm = (state, handleChange) => {
           <Header inverted>Filter By Race</Header>
         </Menu.Item>
         <Menu.Item>
-          <label>
-            White{" "}
-            <input
-              name="Wh"
-              type="checkbox"
-              checked={state.wh}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Black{" "}
-            <input
-              name="Bl"
-              type="checkbox"
-              checked={state.bl}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            LatinX{" "}
-            <input
-              name="Lat"
-              type="checkbox"
-              checked={state.lat}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Asian{" "}
-            <input
-              name="Asn"
-              type="checkbox"
-              checked={state.asn}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            West Asian / North African{" "}
-            <input
-              name="WANA"
-              type="checkbox"
-              checked={state.wana}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            South Asian{" "}
-            <input
-              name="SoAs"
-              type="checkbox"
-              checked={state.soas}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            American Indian{" "}
-            <input
-              name="Amin"
-              type="checkbox"
-              checked={state.amin}
-              onChange={handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Other{" "}
-            <input
-              name="Oth"
-              type="checkbox"
-              checked={state.oth}
-              onChange={handleChange}
-            />
-          </label>
+          <Form inverted>
+            <label>
+              White{" "}
+              <input
+                name="Wh"
+                type="checkbox"
+                checked={state.wh}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Black{" "}
+              <input
+                name="Bl"
+                type="checkbox"
+                checked={state.bl}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              LatinX{" "}
+              <input
+                name="Lat"
+                type="checkbox"
+                checked={state.lat}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Asian{" "}
+              <input
+                name="Asn"
+                type="checkbox"
+                checked={state.asn}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              West Asian / North African{" "}
+              <input
+                name="WANA"
+                type="checkbox"
+                checked={state.wana}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              South Asian{" "}
+              <input
+                name="SoAs"
+                type="checkbox"
+                checked={state.soas}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              American Indian{" "}
+              <input
+                name="Amin"
+                type="checkbox"
+                checked={state.amin}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Other{" "}
+              <input
+                name="Oth"
+                type="checkbox"
+                checked={state.oth}
+                onChange={handleChange}
+              />
+            </label>
+          </Form>
         </Menu.Item>
         <Menu.Item>
           <Header inverted>Filter by genre</Header>
